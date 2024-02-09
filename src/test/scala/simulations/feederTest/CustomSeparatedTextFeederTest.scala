1f925258bf96babd6bbe15a5d4c41822bde2c943
Vignesh Srinivasa Raghavan <40608099+VigneshSrinivasaRaghavan@users.noreply.github.com>
2024-02-09 18:57:34 +0100

    Edited Import Format and added Command Line Code

--- src/test/scala/simulations/feederTest/CustomSeparatedTextFeederTest.scala
+++ src/test/scala/simulations/feederTest/CustomSeparatedTextFeederTest.scala
@@ -1,10 +1,10 @@
 package simulations.feederTest
 
-import io.gatling.core.Predef.*
+import io.gatling.core.Predef._
 import io.gatling.core.feeder.{BatchableFeederBuilder, Feeder}
 import io.gatling.core.scenario.Simulation
 import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
-import io.gatling.http.Predef.*
+import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 
 class CustomSeparatedTextFeederTest extends Simulation {
